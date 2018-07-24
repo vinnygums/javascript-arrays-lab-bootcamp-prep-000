05f5a59872b63d8a8475bfe6e2824507209024cd
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-24 02:00:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function prependKitten(name) {
   return([name, ...kittens])
 }
 function removeLastKitten() {
- return(kittens.slice(0, kittens.length(-1)))
+ return(kittens.slice(0, kittens.length -1 ))
 }
 function removeFirstKitten() {
   return(kittens.slice(1))
